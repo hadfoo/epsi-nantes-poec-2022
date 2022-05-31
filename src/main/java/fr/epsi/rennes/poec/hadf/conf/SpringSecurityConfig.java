2a866bbee56dbfe455ae19720f96325b547d1938
Hadrien <hadrien.fortin@gmail.com>
2022-05-31 15:42:15 +0200

    Spring security : remember me et redirection selon le role

--- src/main/java/fr/epsi/rennes/poec/hadf/conf/SpringSecurityConfig.java
+++ src/main/java/fr/epsi/rennes/poec/hadf/conf/SpringSecurityConfig.java
@@ -1,24 +1,39 @@
 package fr.epsi.rennes.poec.hadf.conf;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.security.authentication.RememberMeAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
-import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+
+import fr.epsi.rennes.poec.hadf.domain.UserRole;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private DataSource ds;
 	
 	public SpringSecurityConfig(@Lazy UserDetailsService userDetailsService) {
 		this.userDetailsService = userDetailsService;
@@ -41,26 +56,45 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.csrf().disable()
 			.formLogin()
-			.defaultSuccessUrl("/user/article-list.html")
+			.successHandler(new AuthenticationSuccessHandler() {
+				// redirige l'utilisateur selon son rôle
+				@Override
+				public void onAuthenticationSuccess(
+						HttpServletRequest request,
+						HttpServletResponse response,
+						Authentication authentication)
+				throws IOException, ServletException {
+					
+					if (authentication.getAuthorities().contains(UserRole.ROLE_USER)) {
+						response.sendRedirect("/user/article-list.html");
+					} else if (authentication.getAuthorities().contains(UserRole.ROLE_ADMIN)) {
+						response.sendRedirect("/admin/article.html");
+					}
+				}
+			})
 		.and()
 			.rememberMe()
-			.rememberMeCookieName("atelier");
+			.rememberMeCookieName("atelier")
+			.tokenRepository(jdbcTokenRepository(ds));
 	}
-
-    @Bean("rememberMeServices")
-    public TokenBasedRememberMeServices rememberMeServices() throws Exception {
-    	return new TokenBasedRememberMeServices("atelier", userDetailsService());
+	
+    @Bean
+    public PersistentTokenBasedRememberMeServices rememberMeServices(DataSource ds) throws Exception {
+    	return new PersistentTokenBasedRememberMeServices("atelier", userDetailsService(), jdbcTokenRepository(ds));
 	}
     
-    @Bean("rememberMeFilter")
-    public RememberMeAuthenticationFilter rememberMeAuthenticationFilter() throws Exception {
+    @Bean
+    public RememberMeAuthenticationFilter rememberMeAuthenticationFilter(DataSource ds) throws Exception {
     	return new RememberMeAuthenticationFilter(
-    			authenticationManagerBean(), rememberMeServices());
+    			authenticationManagerBean(), rememberMeServices(ds));
     }
     
-    @Bean("rememberMeAuthenticationProvider")
-    public RememberMeAuthenticationProvider rememberMeAuthenticationProvider() {
-    	return new RememberMeAuthenticationProvider("atelier");
+    @Bean
+    public JdbcTokenRepositoryImpl jdbcTokenRepository(DataSource ds) {
+    	JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+    	repo.setDataSource(ds);
+    	repo.setCreateTableOnStartup(false);
+    	return repo;
     }
 	
 	@Bean
